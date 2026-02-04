a0e83d26a123c21289d87b0db0fedf69a534234a
Oleksii Iskiv <o.iskiv-c@tamkeentech.sa>
2026-02-04 15:46:57 +0200

    assііasвв

--- fuck.js
+++ fuck.js
@@ -1,71 +1,55 @@
 (function () {
     'use strict';
 
-    var BUTTON_CLASS = 'ua-card-button';
-
-    function addButton(activity) {
-        if (!activity) return;
-
-        var root = activity.render();
-        if (!root || !root.length) return;
-
-        var buttons = root.find('.full-start-new__buttons');
-        if (!buttons.length) return;
-
-        if (buttons.find('.' + BUTTON_CLASS).length) return;
-
-        var btn = $(`
-            <div class="full-start__button selector ${BUTTON_CLASS}"
-                 style="background: rgba(255, 215, 0, 0.25)">
-                <svg width="24" height="24" viewBox="0 0 24 24">
-                    <path d="M12 2L4.5 20.3L5.2 21L12 18L18.8 21L19.5 20.3Z"
-                          fill="gold"/>
+    function startPlugin() {
+        // Додаємо шаблон кнопки (аналогічно до Lampa.Template.add у вашому прикладі)
+        Lampa.Template.add('my_ua_button_template', `
+            <div class="button full-start__button selector my-ua-button">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 2L4.5 20.29L5.21 21L12 18L18.79 21L19.5 20.29L12 2Z" fill="gold"/>
                 </svg>
-                <span>UA</span>
+                <span>Дивитись UA</span>
             </div>
         `);
 
-        btn.on('hover:enter', function () {
-            var movie = activity.movie || {};
-            var title =
-                movie.title ||
-                movie.name ||
-                movie.original_title ||
-                movie.original_name;
+        // Слухаємо подію завантаження картки фільму
+        Lampa.Listener.follow('full', function (e) {
+            // Тип 'complite' означає, що дані завантажені і інтерфейс побудований
+            if (e.type == 'complite') {
+                var container = e.object.find('.full-start__buttons');
+                
+                // Перевіряємо, чи ми вже не додали кнопку
+                if (container.length && !container.find('.my-ua-button').length) {
+                    // Створюємо елемент з шаблону
+                    var btn = Lampa.Template.get('my_ua_button_template');
 
-            if (!title) {
-                Lampa.Noty.show('Немає назви');
-                return;
+                    // Логіка при натисканні
+                    btn.on('hover:enter', function () {
+                        var movie = e.data.movie;
+                        var title = movie.title || movie.name;
+                        
+                        Lampa.Noty.show('Шукаю солов\'їною: ' + title);
+                        
+                        // Відкриваємо пошук (можна замінити на свій парсер)
+                        window.open('https://toloka.to/tracker.php?nm=' + encodeURIComponent(title), '_blank');
+                    });
+
+                    // Додаємо в контейнер
+                    container.append(btn);
+                    
+                    // Оновлюємо контролер, щоб кнопка стала доступною для пульта
+                    Lampa.Controller.render();
+                }
             }
-
-            Lampa.Noty.show('Шукаю українською: ' + title);
-
-            window.open(
-                'https://toloka.to/tracker.php?nm=' +
-                    encodeURIComponent(title),
-                '_blank'
-            );
-        });
-
-        buttons.append(btn);
-
-        if (Lampa.Controller) {
-            Lampa.Controller.render();
-        }
-    }
-
-    function bind() {
-        // ГОЛОВНЕ: full_start
-        Lampa.Listener.follow('full_start', function (e) {
-            if (!e || !e.activity) return;
-            addButton(e.activity);
         });
     }
 
-    if (window.appready) bind();
-    else {
+    // Запуск плагіна тільки коли Lampa готова
+    if (window.appready) {
+        startPlugin();
+    } else {
         Lampa.Listener.follow('app', function (e) {
-            if (e.type === 'ready') bind();
+            if (e.type == 'ready') startPlugin();
         });
     }
 })();
