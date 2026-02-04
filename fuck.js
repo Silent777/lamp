98b5871fe7ef9fcff3cd188dd3b43d263f98b70c
Oleksii Iskiv <o.iskiv-c@tamkeentech.sa>
2026-02-04 15:15:46 +0200

    ass

--- fuck.js
+++ fuck.js
@@ -2,36 +2,42 @@
     'use strict';
 
     function startPlugin() {
-        // Реєструємо компонент
+        // 1. Створюємо компонент сторінки
         Lampa.Component.add('my_plugin', function (object) {
             var comp = new Lampa.Interaction();
             this.create = function () {
-                this.dom = $('<div><h1 style="text-align:center; margin-top:100px;">UA Plugin v3</h1><p style="text-align:center;">Якщо ви це бачите — ви перемогли кеш!</p></div>');
+                this.dom = $('<div><h1 style="text-align:center; margin-top:100px;">UA Plugin v4</h1><p style="text-align:center;">Метод Lampa.Listener спрацював!</p></div>');
                 return this.dom;
             };
             this.render = function () { return this.create(); };
             this.terminate = function () { this.dom.remove(); };
         });
 
-        // Додаємо в меню через невелику паузу
-        setTimeout(function(){
-            Lampa.Menu.add({
-                id: 'my_ua_plugin',
-                title: 'Мій UA Контент',
-                icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>',
-                onSelect: function () {
+        // 2. Додаємо пункт через слухач подій (це надійніше за прямий виклик)
+        Lampa.Listener.follow('app', function (e) {
+            if (e.type == 'ready') {
+                var menu_item = $('<li class="menu__item selector" data-action="my_plugin_action">' +
+                    '<div class="menu__ico">' +
+                        '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line></svg>' +
+                    '</div>' +
+                    '<div class="menu__title">Мій UA Контент</div>' +
+                '</li>');
+
+                menu_item.on('hover:enter', function () {
                     Lampa.Activity.push({
                         url: '',
                         title: 'Мій UA Контент',
                         component: 'my_plugin',
                         page: 1
                     });
-                }
-            });
-        }, 100); 
+                });
+
+                // Додаємо в кінець списку меню
+                $('.menu .menu__list').append(menu_item);
+            }
+        });
     }
 
-    // Більш надійний спосіб перевірки готовності
     if (window.appready) startPlugin();
     else {
         Lampa.Listener.follow('app', function (e) {
